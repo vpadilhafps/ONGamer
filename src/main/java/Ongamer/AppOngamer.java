6942b49ee4176398a9b4c0fa22a9806c5fcfb99f
vpadilhafps <vpadilhabr1@gmail.com>
2020-09-24 20:12:54 -0300

    ATUALIZADO

---
+++ src/main/java/Ongamer/AppOngamer.java
@@ -0,0 +1,23 @@
+package Ongamer;
+
+public class AppOngamer {
+
+    public static void main(String[] args) {
+
+        Usuario Usuario = new Usuario();
+        Login Login = new Login();
+        Doador Doador = new Doador();
+        Matricula Matricula = new Matricula();
+        Aluno Aluno = new Aluno();
+        Professor Professor = new Professor();
+        Diretor Diretor = new Diretor();
+        Endereço Endereço = new Endereço();
+        Telefone Telefone = new Telefone();
+        Escola Escola = new Escola();
+        Turma Turma = new Turma();
+        Materia Materia = new Materia();
+        Atividades Atividades = new Atividades();
+
+        System.out.println("PADILHA DA MULHERADA");
+    }   
+}
---
+++ src/main/java/Ongamer/Materia.java
@@ -0,0 +1,5 @@
+package Ongamer;
+
+public class Materia {
+
+}
